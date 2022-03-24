8d5a64737070b7ee64b28750b0be67f805005c27
Aaron Saula <n00201123@iadt.ie>
2022-03-24 11:38:23 +0000

    SeedersAdded

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Hash;
+use App\Models\Role;
+use App\Models\User;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -14,6 +18,23 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = 'Aaron Saula';
+        $admin->email = 'aaron@larafest.ie';
+        $admin->password = Hash::make('password');
+        $admin->save();
+
+        $admin->roles()->attach($role_admin);
+        
+        $user = new User();
+        $user->name = 'John Jones';
+        $user->email = 'joe@larafest.ie';
+        $user->password = Hash::make('password');
+        $user->save();
+        
+        $user->roles()->attach($role_user);
     }
 }
